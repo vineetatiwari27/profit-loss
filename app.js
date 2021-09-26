1a86e3fd0630dfdb7cdcf4646d77dc9c278897be
vineetatiwari27 <85996556+vineetatiwari27@users.noreply.github.com>
2021-09-26 20:03:41 +0530

    docs: changes in documentation

---
+++ app.js
@@ -0,0 +1,34 @@
+var cp = document.querySelector("#initial");
+var stock = document.querySelector("#quan");
+var sp = document.querySelector("#curr");
+var btnInfo = document.querySelector("#btn");
+var info = document.querySelector("#calc");
+btnInfo.addEventListener("click", function eventHandler() {
+    var costPrice = cp.value;
+    var sellingPrice = sp.value;
+    var st = stock.value;
+    if (costPrice > sellingPrice) {
+        var loss = costPrice - sellingPrice;
+        var lossTotal = loss * st;
+        var losspercent = (lossTotal * 100) / costPrice;
+        information("The loss is: " + lossTotal + " and percent is: " + losspercent);
+    }
+    else if (costPrice < sellingPrice) {
+        var profit = sellingPrice - costPrice;
+        var profitTotal = profit * st;
+        var profitPercent = (profitTotal * 100) / costPrice;
+        information("The profit is: " + profitTotal + " and percent is: " + profitPercent);
+    }
+    else if (costPrice == sellingPrice) {
+
+        information("NOTHING! OOPS");
+    }
+    else {
+        information("Check your data again!")
+    }
+
+})
+function information(msg) {
+    info.style.display = "block";
+    info.innerText = msg;
+}
